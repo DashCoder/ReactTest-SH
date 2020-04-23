c1988d0a3e18f0a0665e0277dbf3e528a8966a0b
Jon Axel Sunnehall <axels@dashup.co>
2020-04-23 13:39:03 +0100

    QuestionOne issue fixed

--- src/pages/questionOne/questionOne.js
+++ src/pages/questionOne/questionOne.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
+/* 
+Comments: 
+
+1) Use of standard ES6 function component instead of React.Component where possible, state supported by useState 
+2) Use ES6 Spread Operator to maintain label value
+
+Note: 
+Currently Using react 16.10.2. 
+Hooks are a new addition in React 16.8. Hooks let you use state and other React features without using a react class. 
+
+*/
+
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 
-class QuestionOne extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			label: "I've been clicked: ",
-			counter: 0
-		}
-	}
-	render(){
-		const handleOnClick = () =>{
-			this.setState({
-				counter: this.state.counter + 1
-			})
-		};
-		return (
-			<div style={{marginTop: 48}}>
-				<Button variant="contained" onClick={handleOnClick()}  >
-					{this.state.label} {this.state.counter} times
-				</Button>
-			</div>
-		)
-	}
-}
+const QuestionOne = () => {
+ 
+  const [state, setState] = useState({
+  	 label: "I've been clicked: ",
+	 counter: 0
+  });
 
-export default QuestionOne
+  const handleOnClick = () => {
+    setState({
+      ...state,
+	  counter: state.counter + 1
+    });
+  };
+
+  return (
+	<div style={{marginTop: 48}}>
+		<Button variant="contained" onClick={handleOnClick}  >
+			{state.label} {state.counter} times
+		</Button>
+	</div>
+  );
+
+};
+
+
+export default QuestionOne;
